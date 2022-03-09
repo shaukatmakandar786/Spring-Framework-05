32c5fe916198befb4ae0a855a306b89941c5048f
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2022-03-09 23:21:07 +0530

    added

---
+++ spring-core-eaxmples/app04.3/src/com/shaukat/test/Test.java
@@ -0,0 +1,19 @@
+package com.shaukat.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.shaukat.beans.HelloBean;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/shaukat/resourses/applicationContext.xml");
+		System.out.println(context.getBean("helloBean1"));
+		System.out.println(context.getBean("helloBean1"));
+		System.out.println(context.getBean("helloBean1"));
+		
+	}
+
+}
