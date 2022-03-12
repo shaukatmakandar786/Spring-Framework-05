f8a9163d5820000a3f4b5f47f1e3101f03b36e20
shaukatmakandar786 <shaukatmakandar786@gmail.com>
2022-03-12 15:34:46 +0530

    added bean instantiating by suing static factory method

---
+++ spring-core-eaxmples/app04.6/src/com/shaukat/beans/HelloBean.java
@@ -0,0 +1,24 @@
+package com.shaukat.beans;
+
+public class HelloBean {
+
+	static {
+		System.out.println("Hello bean class loading");
+	}
+	public HelloBean()
+	{
+		System.out.println("HelloBean class Instantiating");
+	}
+	
+	public String sayHello()
+	{
+		return "Hello Guys...!";
+	}
+	
+	public static HelloBean getInstance()
+	{
+		System.out.println("static factory method");
+		return new HelloBean();
+	}
+	
+}
